8c9da830f91dccf368eeeb47a2e86e74cb087c5b
2382546457 <2382546457@qq.com>
2023-06-13 11:08:00 +0800

    登录注册功能

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,7 +1,11 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -11,6 +15,26 @@ import com.hmdp.entity.User;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Service
 public interface IUserService extends IService<User> {
+
+    /**
+     * 发送验证码
+     * @param phone 目标手机号
+     * @param session 验证码存入session
+     * @return 是否成功
+     */
+    public Result sendCode(String phone, HttpSession session);
+
+    /**
+     * 用户登录
+     * @param phone 手机号
+     * @param password 密码
+     * @param code 验证码
+     * @param session session
+     * @return 是否陈成功
+     */
+    Result login(String phone, String password, String code, HttpSession session);
+
 
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.PasswordEncoder;
+import com.hmdp.utils.SystemConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 /**
  * <p>
@@ -15,6 +28,53 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public Result sendCode(String phone, HttpSession session) {
+        // 生成验证码
+        String code = RandomUtil.randomString(6);
+        // 存入session
+        session.setAttribute("code", code);
+        log.debug("验证码: {}", code);
+        // 返回
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(String phone, String password, String code, HttpSession session) {
+        // 1. 校验验证码
+        String code1 = (String) session.getAttribute("code");
+        if (Strings.isEmpty(code1) || !code1.equals(code)) {
+            return Result.fail("验证码错误");
+        }
+        // 2.如果数据库中没有该用户，注册
+        User user = query().eq("phone", phone).one();
+        if (Objects.isNull(user)) {
+            // 注册
+            user = createUserWithPhone(phone);
+        }
+        session.setAttribute("user", user);
+
+        return Result.ok();
+    }
+
+    /**
+     * 根据手机号创建用户
+     *
+     * @param phone 手机号
+     * @return
+     */
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        // 数据库保存用户
+        save(user);
+        return user;
+    }
 
 }
